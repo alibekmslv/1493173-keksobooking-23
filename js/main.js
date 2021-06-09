e6ddcb3075c68b6529f7a3aa17891bc3379dcc31
Alibek <alibekmslv@gmail.com>
2021-06-09 14:20:20 +0300

    Создана функция по гененрированию объявлений

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function getRandomInt(min, max) {
 }
 
 
-function getRandomFloatingPoint (min, max, digits) {
+function getRandomFloatingPoint(min, max, digits) {
   if (min < 0 || max < 0) {
     return null;
   }
@@ -46,3 +46,75 @@ function getRandomFloatingPoint (min, max, digits) {
 
 getRandomInt();
 getRandomFloatingPoint();
+
+const GENERATED_OFFERS_COUNT = 10;
+const OFFER_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN_OPTIONS = ['12:00', '13:00', '14:00'];
+const OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const OFFER_PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const PHOTO_COUNTER = {
+  leadingInt: 0,
+  countedInt: 0,
+}
+
+const getPhotoNumber = () => {
+  PHOTO_COUNTER.countedInt++;
+
+  if (PHOTO_COUNTER.countedInt >= 9) {
+    PHOTO_COUNTER.leadingInt++;
+    PHOTO_COUNTER.countedInt = 0;
+  }
+
+  return `${PHOTO_COUNTER.leadingInt}${PHOTO_COUNTER.countedInt}`;
+}
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+const getRandomArrayShuffledElements = (elements) => {
+  const result = [];
+  const maxIterations = elements.length * 3;
+  const randomIterationsNumber = getRandomPositiveInteger(1, maxIterations);
+
+  for (let i = 0; i < randomIterationsNumber; i++) {
+    const arrayLengthRandomInteger = getRandomPositiveInteger(0, elements.length - 1);
+    if (!result.includes(elements[arrayLengthRandomInteger])) {
+      result.push(elements[arrayLengthRandomInteger]);
+    }
+  }
+
+  return result;
+}
+
+const generateOffer = () => {
+  const offerRandomLat = getRandomPositiveFloat(35.65000, 35.70000, 5);
+  const offerRandomLng = getRandomPositiveFloat(139.70000, 139.80000, 5);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${getPhotoNumber()}.png`,
+    },
+    offer: {
+      title: 'Квартира в Токио',
+      address: `${offerRandomLat}, ${offerRandomLng}`,
+      price: getRandomPositiveInteger(1, 50000),
+      type: getRandomArrayElement(OFFER_TYPES),
+      rooms: getRandomPositiveInteger(1, 30),
+      guests: getRandomPositiveInteger(1, 60),
+      checkin: getRandomArrayElement(CHECKIN_OPTIONS),
+      checkout: getRandomArrayElement(CHECKIN_OPTIONS),
+      features: getRandomArrayShuffledElements(OFFER_FEATURES, 21),
+      description: 'Уютная квартира в Токио',
+      photos: getRandomArrayShuffledElements(OFFER_PHOTOS, 6),
+    },
+    location: {
+      lat: offerRandomLat,
+      lng: offerRandomLng,
+    }
+  }
+}
+
+const generatedOffers = new Array(GENERATED_OFFERS_COUNT).fill(null).map(() => {
+  return generateOffer();
+})
